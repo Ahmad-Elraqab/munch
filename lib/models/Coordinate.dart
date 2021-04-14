c910a4cb9b73a0d0cb2eea7ce78157b8e186b11b
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2021-04-14 08:04:11 +0800

    bug

---
+++ lib/models/Coordinate.dart
@@ -0,0 +1,26 @@
+import 'package:munch_app/models/picture.dart';
+
+class Coordinate {
+  int id;
+  List<String> latitude;
+  List<String> longitude;
+  Coordinate({
+    this.id,
+    this.latitude,
+    this.longitude,
+  });
+  Coordinate.fromJson(Map<String, dynamic> json)
+      : this(
+          latitude: json['ZoneCoordinates']
+              .toString()
+              .split('0')
+              .toList()
+              .map((e) => e.split(',')[0]),
+          longitude: json['ZoneCoordinates']
+              .toString()
+              .split('0')
+              .toList()
+              .map((e) => e.split(',')[1]),
+        );
+  Map<String, dynamic> toJson() => {};
+}
--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -6,6 +6,20 @@ class CartProvider extends ChangeNotifier {
   Map products;
   int itemsCount;
 
+// {
+// 	"CustomerId":"32548",
+// 	"CustomerGuid":"9E4C448C-A45E-4C75-A557-37193354DC86",
+// 	"StoreId":"1",
+// 	"Price":"5.00",
+// 	"Quantity":"10",
+// 	"ProductId":2,
+// 	"MarkAsDeleted":0,
+// 	"ShoppingCartId":-1,
+// 	"OrderDate":"7/11/2020",
+// 	"CityId":"-1",
+// 	"DistrictId":"-1",
+// 	"ZoneId":"3"
+// }
   void addToCart(Product product) {
     if (!products.containsKey(product)) {
       products[product] = 1;
